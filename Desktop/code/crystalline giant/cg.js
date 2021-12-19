a663691f441835981b6a38d50c7b23834e80afdc
Eric Brooks <ericbrookspro@gmail.com>
2021-12-18 23:59:32 -0600

    new start

---
+++ Desktop/code/crystalline giant/cg.js
@@ -0,0 +1,10 @@
+const currentAbilities = []
+
+// create list of possibilities
+let allAbilities = ['flying', 'first strike', 'deathtouch', 'hexproof', 'lifelink', 'menace', 'reach', 'trample', 'vigilance', '+1/+1']
+
+// filter list
+
+// pick random from filtered list
+
+// add picked ability to current abilities
